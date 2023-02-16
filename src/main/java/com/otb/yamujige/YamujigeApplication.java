cadd6b5da1ad275e0dfa989aeeb9b40220494d82
thekim123 <tacdo12@naver.com>
2023-02-16 23:57:42 +0900

    프로젝트 생성

---
+++ src/main/java/com/otb/yamujige/YamujigeApplication.java
@@ -0,0 +1,13 @@
+package com.otb.yamujige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamujigeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamujigeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otb/yamujige/YamujigeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otb.yamujige;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamujigeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
